cec7c97b498c775f195031b3d491b6de8d5d5973
vnresende <96209492+vnresende@users.noreply.github.com>
2023-02-06 20:32:43 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -2,3 +2,11 @@ def soma(a, b):
       return a + b
 def mult(a, b):
       return a * b
+
+
+def importar():
+      from google.colab import files
+      uploaded = files.upload()
+      filename = list(uploaded.keys())[0]
+      output_filename = filename.replace('.xlsx', ".mf4")
+      return (filename, output_filename)
