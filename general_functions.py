5ba8d797a566d4cdf65e216564e9486e587a68d3
vnresende <96209492+vnresende@users.noreply.github.com>
2023-02-06 21:03:16 -0300

    Update and rename funcoes.py to general_functions.py

---
+++ general_functions.py
@@ -0,0 +1,8 @@
+def uploading_files():
+    from google.colab import files
+    uploaded = files.upload()
+    uploaded_files = list(uploaded.keys())
+    output_filename = [] 
+    for file_name in uploaded_files:
+        output_filename.append(file_name.replace('.xlsx', ".mf4"))
+    return (uploaded_files, output_filename)
