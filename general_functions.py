4889b800a037a9cac691d1121a00872b53769cc1
vnresende <96209492+vnresende@users.noreply.github.com>
2023-02-10 08:29:41 -0300

    Update general_functions.py

--- general_functions.py
+++ general_functions.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import numpy as np
+import math
+!pip install asammdf==7.2.0
+from asammdf import MDF, Signal
+
 def uploading_files():
     from google.colab import files
     uploaded = files.upload()
