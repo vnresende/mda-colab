e2f4d5874eed51b38aa80f0e05e419d1bda0c2fc
vnresende <96209492+vnresende@users.noreply.github.com>
2023-02-10 10:45:36 -0300

    Update general_functions.py

--- general_functions.py
+++ general_functions.py
@@ -1,5 +1,5 @@
 def uploading_files():
-    from google.colab import files
+    from google.colab import files, runtime
     uploaded = files.upload()
     uploaded_files = list(uploaded.keys())
     output_filename = [] 
