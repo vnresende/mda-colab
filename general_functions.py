186bfa1ed914df1c2ba696d04eacd92ed9a35de9
vnresende <96209492+vnresende@users.noreply.github.com>
2023-02-13 10:09:28 -0300

    Update general_functions.py

--- general_functions.py
+++ general_functions.py
@@ -1,5 +1,5 @@
 def uploading_files():
-    """Realiza o upload de varios arquivos simultaneamente."""
+    """type: tuple. Realiza o upload de varios arquivos simultaneamente."""
     from google.colab import files
     uploaded = files.upload()
     uploaded_files = list(uploaded.keys())
@@ -8,8 +8,15 @@ def uploading_files():
         output_filename.append(file_name.replace('.xlsx', ".mf4"))
     return (uploaded_files, output_filename)
 
+def uploading_single_file():
+    """type: list. Realiza o upload de um único arquivo por vez."""
+    from google.colab import files
+    uploaded = files.upload()
+    uploaded_file = list(uploaded.keys())[0]
+    return uploaded_file
+
 def subir_aquivo():
-    """Realiza o upload de um único arquivo por vez."""
+    """type: boolean. Validação de loop para subir arquivos"""
     repetir = ''
     while repetir.lower() != 's' and repetir.lower() != 'n':
         repetir = input('Deseja adicionar mais algum arquivo? s ou  n \n')
