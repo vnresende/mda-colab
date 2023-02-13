832153a23752fae7426b8c1d71392488076b21c4
vnresende <96209492+vnresende@users.noreply.github.com>
2023-02-13 10:07:58 -0300

    Update general_functions.py

--- general_functions.py
+++ general_functions.py
@@ -1,4 +1,5 @@
 def uploading_files():
+    """Realiza o upload de varios arquivos simultaneamente."""
     from google.colab import files
     uploaded = files.upload()
     uploaded_files = list(uploaded.keys())
@@ -6,3 +7,16 @@ def uploading_files():
     for file_name in uploaded_files:
         output_filename.append(file_name.replace('.xlsx', ".mf4"))
     return (uploaded_files, output_filename)
+
+def subir_aquivo():
+    """Realiza o upload de um único arquivo por vez."""
+    repetir = ''
+    while repetir.lower() != 's' and repetir.lower() != 'n':
+        repetir = input('Deseja adicionar mais algum arquivo? s ou  n \n')
+        if repetir.lower() != 's' and repetir.lower() != 'n':
+            print('caractere inválido')
+
+    if repetir.lower() == 's':
+        return True
+    elif repetir.lower() == 'n':
+        return False
