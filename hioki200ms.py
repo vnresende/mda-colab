daa3c9217973150b90d0bb221976084e279fd6ac
vnresende <96209492+vnresende@users.noreply.github.com>
2023-03-06 08:20:29 -0300

    Update hioki200ms.py

--- hioki200ms.py
+++ hioki200ms.py
@@ -39,11 +39,19 @@ def hioki200ms(files):
 # Using graph_objects
 import plotly.express as px
 
-def plot_hioki_graph(df_avg, df_avg_show):
-    fig = px.line(df_avg, x ='datetime', y = df_avg_show.columns,
-                hover_data={'datetime': '| %d/%m \n%H:%M:%S'},
-                title='Tensão e Corrente')
-    fig.update_xaxes(tickformat='%d/%m \n%H:%M:%S')
+def plot_hioki_graph(df_avg, df_avg_show, title_name, new_labels_name):
+    fig = px.line(df_avg, x = 'datetime', y = df_avg_show.columns,
+                hover_data = {'datetime': '| %d/%m \n%H:%M:%S'},
+                title = title_name
+                labels = {'variable': 'Legenda', 'datetime' : ''})
+    fig.update_xaxes(tickformat = '%d/%m \n%H:%M:%S')
 
+    fig.for_each_trace(lambda t: t.update(name = new_labels_name[t.name],
+                                        legendgroup = new_labels_name[t.name],
+                                        hovertemplate = t.hovertemplate.replace(t.name, new_labels_name[t.name])
+                                         )
+                      )
     fig.show()
+    fig.write_html(title_name + '.html')
+    return title_name + '.html'
 
